c1bfe98aefe63651b9275bc9d29a79caa9e5cce8
Gage Morgan <gage@gages.blog>
2020-03-05 01:41:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase
+  if phrase != phrase.upcase
     puts "HUH? SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
