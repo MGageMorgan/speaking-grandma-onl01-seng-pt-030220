4337a221cd782db89d788054209e5491a56fc8fa
Gage Morgan <gage@gages.blog>
2020-03-05 01:37:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,10 +16,10 @@ def speak_to_grandma(phrase)
   # if phrase != phrase.uppercase
     # puts "HUH? SPEAK UP, SONNY!"
   if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH? SPEAK UP, SONNY!"
+    return "HUH? SPEAK UP, SONNY!"
   end
 end
